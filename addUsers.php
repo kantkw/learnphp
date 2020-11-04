faf57e0d6ea5cf59c41d3aec42cd0dafed1ad1dc
kantkw <kan.tkw@oundleschool.org.uk>
2020-11-04 12:36:37 +0000

    commit10

--- addUsers.php
+++ addUsers.php
@@ -1,3 +1,16 @@
 <?php
 echo $_POST["gender"];
+
+include_once("connection.php");
+
+switch($_POST["role"]){
+    case "Pupil":
+        $role=0;
+        break;
+    case "Teacher":
+        echo"asd"
+    case "Other":
+        $role=1;
+        break;
+}
 ?>
--- connection.php
+++ connection.php
@@ -1,5 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "mydb";
+$server_name = "localhost";
+$username = "root";
+$password = "";
+$dbname = "options";
+
+try {
+    $conn = new PDO("mysql:host=$server_name;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
