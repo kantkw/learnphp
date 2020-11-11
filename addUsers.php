5f1d42638370d1937ba7a7249639fb6deafe5311
kantkw <kan.tkw@oundleschool.org.uk>
2020-11-11 10:07:33 +0000

    commit11

--- addUsers.php
+++ addUsers.php
@@ -1,5 +1,5 @@
 <?php
-echo $_POST["gender"];
+echo $_POST["pwd"];
 
 include_once("connection.php");
 
@@ -8,9 +8,24 @@ switch($_POST["role"]){
         $role=0;
         break;
     case "Teacher":
-        echo"asd"
-    case "Other":
         $role=1;
         break;
+    case "Admin":
+        $role=2;
+        break;
 }
+
+$stmt = $conn->prepare("INSERT INTO tblusers
+(UserID,Gender,Surname,Forename,Password,House,Year,Role) VALUES
+(null,:gender,:surname,:forename,:password,:house,:year,:role)");
+
+$stmt->bindParam(":forename", $_POST["forename"]);
+$stmt->bindParam(":surname", $_POST["surname"]);
+$stmt->bindParam(":house", $_POST["house"]);
+$stmt->bindParam(":password", $_POST["pwd"]);
+$stmt->bindParam(":gender", $_POST["gender"]);
+$stmt->bindParam(":year", $_POST["year"]);
+$stmt->bindParam(":role", $role);
+$stmt->execute();
+$conn=null;
 ?>
